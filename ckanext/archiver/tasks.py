e16946dd30900847a37f082b746ef059f80c9149
Abdel Rahman Hussein <abdelrahman146@outlook.com>
2019-11-28 10:35:25 +0400

    Update tasks.py
    updated to utf-8

--- ckanext/archiver/tasks.py
+++ ckanext/archiver/tasks.py
@@ -590,7 +590,7 @@ def archive_resource(context, resource, log, result=None, url_timeout=30):
     try:
         file_name = parsed_url.path.split('/')[-1] or 'resource'
         file_name = file_name.strip()  # trailing spaces cause problems
-        file_name = file_name.encode('utf-8', 'ignore')  # e.g. u'\xa3' signs
+        file_name = file_name.encode('utf-8')  # e.g. u'\xa3' signs
     except Exception:
         file_name = "resource"
 
